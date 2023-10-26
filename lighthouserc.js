f3bb7bcf39b789d273cd766cb4a9c727ef25ead0
Tanner <tannercdolby@gmail.com>
2023-10-25 19:39:38 -0700

    feat: add example of using static exports with Next.js and lighthouse CI

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,9 +1,11 @@
 module.exports = {
   ci: {
-    collect: {
-      url: ['http://localhost:3000/'],
-      startServerCommand: 'npm run start',
-    },
+    // TODO: uncomment custom server when output: 'export'
+    // is removed from next.config.js
+    // collect: {
+    //   url: ['http://localhost:3000/'],
+    //   startServerCommand: 'npm run start',
+    // },
     upload: {
       target: 'temporary-public-storage',
     },
